c739f1f78734c4c3556c7b1d11cf55dce1d5378c
Aditya-Quantic <asen@getquantic.com>
2024-06-27 13:46:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountName = "Addy"
+var age = 20
+accountCity = "Doon"
+accountCity = "delhi"
+let accountNo;
+
+console.table([accountId,accountName,age,accountCity,accountNo])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+'use strict'  // to use newer version of javascript
+
+let name = "Aditya"
+let age = 20
+let isMale = true
+let data = null
+let abc = undefined
+
+console.log(typeof abc);
