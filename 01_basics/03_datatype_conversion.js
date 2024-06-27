ddb9ab08b0e52131bb5ef922753665af3df6ca54
Aditya-Quantic <asen@getquantic.com>
2024-06-27 14:17:26 +0000

    datatype conversion

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,21 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let variableInNumber = Number(score)
+
+// console.log(variableInNumber);
+
+// let data = "33ab"
+// let dat2 = null = 0 , Object
+// let data3 = undefined = Nan, undefined
+
+let loggedIn = 0
+
+let dataInBoolean = Boolean(loggedIn)
+
+console.log(typeof(dataInBoolean),dataInBoolean);
+
+// "" = false , "adfg" = true,
+// 1 = true , 0 = false
