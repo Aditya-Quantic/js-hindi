e83e2c05d10460c6ba8649d258288245f512436e
Aditya-Quantic <asen@getquantic.com>
2024-06-28 14:14:54 +0000

    operators

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -15,7 +15,34 @@ let loggedIn = 0
 
 let dataInBoolean = Boolean(loggedIn)
 
-console.log(typeof(dataInBoolean),dataInBoolean);
+// console.log(typeof(dataInBoolean),dataInBoolean);
 
 // "" = false , "adfg" = true,
 // 1 = true , 0 = false
+
+// operations
+
+let value = "add" 
+let negvalue = -value
+
+// console.log(typeof negvalue,negvalue);
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2");//32
+
+// let gamechanger = 400;
+// gamechanger++
+// console.log(gamechanger);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
